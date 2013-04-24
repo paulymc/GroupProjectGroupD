c2acc078a9cb275f063be316229189d07f0bf6d6
Paul McKenna <paul.p.mckenna12@gmail.com>
2013-04-24 10:28:38 +0100

    Final version, one been submitted

---
+++ Rental_System_v1.8/Rental System/src/Model_test.java
@@ -0,0 +1,29 @@
+//import java.text.DateFormat;
+//import java.text.SimpleDateFormat;
+//import java.util.Date;
+
+import java.util.Calendar;
+
+public class Model_test
+{
+
+	/*public static void main(String[] args) 
+	{
+		
+		Calendar now = Calendar.getInstance();
+	    System.out.println("Current date : " + (now.get(Calendar.MONTH) + 1) + "-"
+	        + now.get(Calendar.DATE) + "-" + now.get(Calendar.YEAR));
+
+	    // add days to current date using Calendar.add method
+	    now.add(Calendar.DATE, 9);
+
+	    String return_date = "date after one day : " + (now.get(Calendar.MONTH) + 1) + "-" + now.get(Calendar.DATE) + "-" + now.get(Calendar.YEAR);
+		//DateFormat f = new SimpleDateFormat("yyyy-MM-dd");
+		//Date now = new Date();
+		System.out.println(return_date);
+		}*/
+	
+
+}
+
+
